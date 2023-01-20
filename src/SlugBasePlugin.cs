43122fb0999fc4ef847cb8f37c1bc8baa9740f1f
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-01-20 16:03:39 -0500

    Use SDK project, use src/ lib/ and asset/

---
+++ src/SlugBasePlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BepInEx;
+
+namespace SlugBase
+{
+    [BepInPlugin("slime-cubed.slugbase", "SlugBase", "2.0.0")]
+    internal class SlugBasePlugin : BaseUnityPlugin
+    {
+        private bool initialized = false;
+
+        public void Awake()
+        {
+            On.RainWorld.OnModsInit += (orig, self) =>
+            {
+                orig(self);
+
+                if (initialized) return;
+                initialized = true;
+
+                // Features.PlayerFeatures.Register("explode_on_death", (p, s) => new ExplodeOnDeath(p, s));
+            };
+        }
+    }
+}
