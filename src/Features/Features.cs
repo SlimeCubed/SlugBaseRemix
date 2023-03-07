d6d85a3eea3d272d8b1d124d0bce45598124c60e
SlimeCubed <cubedslime@gmail.com>
2023-03-06 17:00:18 -0800

    Remove TODOs

--- src/Features/Features.cs
+++ src/Features/Features.cs
@@ -13,51 +13,39 @@ namespace SlugBase.Features
     /// </summary>
     public static class PlayerFeatures
     {
-        // TODO: Test
         /// <summary>"color": Player body and UI color.</summary>
         public static readonly PlayerFeature<Color> SlugcatColor = PlayerColor("color");
 
-        // TODO: Test
         /// <summary>"auto_grab_batflies": Grab batflies on collision.</summary>
         public static readonly PlayerFeature<bool> AutoGrabFlies = PlayerBool("auto_grab_batflies");
 
-        // TODO: Test
         /// <summary>"weight": Weight multiplier.</summary>
         public static readonly PlayerFeature<float[]> WeightMul = PlayerFloats("weight", 1, 2);
 
-        // TODO: Test
         /// <summary>"tunnel_speed": Move speed in tunnels.</summary>
         public static readonly PlayerFeature<float[]> TunnelSpeedMul = PlayerFloats("tunnel_speed", 1, 2);
 
-        // TODO: Test
         /// <summary>"climb_speed": Move speed on poles.</summary>
         public static readonly PlayerFeature<float[]> ClimbSpeedMul = PlayerFloats("climb_speed", 1, 2);
 
-        // TODO: Test
         /// <summary>"walk_speed": Standing move speed.</summary>
         public static readonly PlayerFeature<float[]> WalkSpeedMul = PlayerFloats("walk_speed", 1, 2);
 
-        // TODO: Test
         /// <summary>"crouch_stealth": Visual stealth while crouched.</summary>
         public static readonly PlayerFeature<float[]> CrouchStealth = PlayerFloats("crouch_stealth", 1, 2);
 
-        // TODO: Test
         /// <summary>"throw_skill": Spear damage and speed.</summary>
         public static readonly PlayerFeature<int[]> ThrowSkill = PlayerInts("throw_skill", 1, 2);
 
-        // TODO: Test
         /// <summary>"lung_capacity": Time underwater before drowning.</summary>
         public static readonly PlayerFeature<float[]> LungsCapacityMul = PlayerFloats("lung_capacity", 1, 2);
 
-        // TODO: Test
         /// <summary>"loudness": Sound alert multiplier.</summary>
         public static readonly PlayerFeature<float[]> LoudnessMul = PlayerFloats("loudness", 1, 2);
 
-        // TODO: Test
         /// <summary>"back_spear": Store a spear on back.</summary>
         public static readonly PlayerFeature<bool> BackSpear = PlayerBool("back_spear");
         
-        // TODO: Test
         /// <summary>"alignments": Initial community reputation.</summary>
         public static readonly PlayerFeature<Dictionary<CreatureCommunities.CommunityID, RepOverride>> CommunityAlignments = new("alignments", json =>
         {
@@ -71,11 +59,9 @@ namespace SlugBase.Features
             return reps;
         });
 
-        // TODO: Test base
         /// <summary>"diet": Edibility and nourishment of foods.</summary>
         public static readonly PlayerFeature<Diet> Diet = new("diet", json => new Diet(json));
 
-        // TODO: Test
         /// <summary>"custom_colors": Configurable player colors.</summary>
         public static readonly PlayerFeature<ColorSlot[]> CustomColors = new("custom_colors", json =>
         {
@@ -110,67 +96,51 @@ namespace SlugBase.Features
     /// </summary>
     public static class GameFeatures
     {
-        // TODO: Test
         /// <summary>"karma": Initial karma.</summary>
         public static readonly GameFeature<int> Karma = GameInt("karma");
 
-        // TODO: Test
         /// <summary>"karma_cap": Initial karma cap.</summary>
         public static readonly GameFeature<int> KarmaCap = GameInt("karma_cap");
 
-        // TODO: Test
         /// <summary>"start_room": Initial room, plus backups from highest to lowest priority.</summary>
         public static readonly GameFeature<string[]> StartRoom = GameStrings("start_room", 1);
 
-        // TODO: Test
         /// <summary>"guide_overseer": Player guide overseer color index.</summary>
         public static readonly GameFeature<int> GuideOverseer = GameInt("guide_overseer");
 
-        // TODO: Test
         /// <summary>"has_dreams": Whether or not to track dream state.</summary>
         public static readonly GameFeature<bool> HasDreams = GameBool("has_dreams");
 
-        // TODO: Test
         /// <summary>"cycle_length_min": Minimum cycle length in minutes.</summary>
         public static readonly GameFeature<float> CycleLengthMin = GameFloat("cycle_length_min");
 
-        // TODO: Test
         /// <summary>"cycle_length_max": Maximum cycle length in minutes.</summary>
         public static readonly GameFeature<float> CycleLengthMax = GameFloat("cycle_length_max");
 
-        // TODO: Test
         /// <summary>"perma_unlock_gates": Maximum cycle length in minutes.</summary>
         public static readonly GameFeature<bool> PermaUnlockGates = GameBool("perma_unlock_gates");
 
-        // TODO: Test
         /// <summary>"food_min": Food needed to sleep.</summary>
         public static readonly GameFeature<int> FoodMin = GameInt("food_min");
 
-        // TODO: Test
         /// <summary>"food_max": Maximum food stored during a cycle.</summary>
         public static readonly GameFeature<int> FoodMax = GameInt("food_max");
 
-        // TODO: Test
         /// <summary>"select_menu_scene": The scene for this slugcat on the select menu.</summary>
         public static readonly GameFeature<MenuScene.SceneID> SelectMenuScene = GameExtEnum<MenuScene.SceneID>("select_menu_scene");
 
-        // TODO: Test
         /// <summary>"select_menu_scene_ascended": The scene for this slugcat on the select menu when ascended.</summary>
         public static readonly GameFeature<MenuScene.SceneID> SelectMenuSceneAscended = GameExtEnum<MenuScene.SceneID>("select_menu_scene_ascended");
 
-        // TODO: Test
         /// <summary>"sleep_scene": The scene for this slugcat when hibernating.</summary>
         public static readonly GameFeature<MenuScene.SceneID> SleepScene = GameExtEnum<MenuScene.SceneID>("sleep_scene");
 
-        // TODO: Test
         /// <summary>"starve_scene": The scene for this slugcat when losing from starvation.</summary>
         public static readonly GameFeature<MenuScene.SceneID> StarveScene = GameExtEnum<MenuScene.SceneID>("starve_scene");
 
-        // TODO: Test
         /// <summary>"death_scene": The scene for this slugcat when losing from a non-starvation death.</summary>
         public static readonly GameFeature<MenuScene.SceneID> DeathScene = GameExtEnum<MenuScene.SceneID>("death_scene");
 
-        // TODO: Test
         /// <summary>"world_state": The character to use for creature spawns and room connections.</summary>
         public static readonly GameFeature<SlugcatStats.Name[]> WorldState = new("world_state", json =>
         {
