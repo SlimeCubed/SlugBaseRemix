a68cffcfb39e4b0c7fbdced275a515ad955353df
SlimeCubed <cubedslime@gmail.com>
2023-04-05 12:58:39 -0700

    Use full path as atlas name for custom scenes

--- src/Assets/AssetHooks.cs
+++ src/Assets/AssetHooks.cs
@@ -1,13 +1,42 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Schema;
 using Menu;
+using MonoMod.Cil;
 
 namespace SlugBase.Assets
 {
     internal static class AssetHooks
     {
+        private const string SLUGBASE_FOLDER = "SlugBase Assets";
+
         public static void Apply()
         {
+            IL.Menu.MenuIllustration.LoadFile_string += MenuIllustration_LoadFile_string;
             On.Menu.MenuScene.BuildScene += MenuScene_BuildScene;
+        }
+
+        // Use paths as atlas names instead of just the file name
+        private static void MenuIllustration_LoadFile_string(ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            if(c.TryGotoNext(MoveType.Before,
+                x => x.MatchCallOrCallvirt<AssetManager>(nameof(AssetManager.ResolveFilePath))))
+            {
+                c.EmitDelegate<Func<string, string>>(path =>
+                {
+                    if(path.StartsWith(SLUGBASE_FOLDER + Path.DirectorySeparatorChar))
+                        return path.Substring(SLUGBASE_FOLDER.Length + 1);
+                    else
+                        return path;
+                });
+            }
+            else
+            {
+                SlugBasePlugin.Logger.LogError($"IL hook {nameof(MenuIllustration_LoadFile_string)} failed!");
+            }
         }
 
         // Override building custom scenes
@@ -21,15 +50,15 @@ namespace SlugBase.Assets
                 bool flatmode = self.flatMode && customScene.Images.Any(img => img.Flatmode);
                 self.sceneFolder = customScene.SceneFolder ?? "";
 
-                foreach(var img in customScene.Images)
+                foreach (var img in customScene.Images)
                 {
                     if (img.Flatmode != flatmode)
                         continue;
 
                     if (img.Depth != -1f)
-                        self.AddIllustration(new MenuDepthIllustration(self.menu, self, self.sceneFolder, img.Name, img.Position, img.Depth, img.Shader));
+                        self.AddIllustration(new MenuDepthIllustration(self.menu, self, SLUGBASE_FOLDER, Path.Combine(self.sceneFolder, img.Name), img.Position, img.Depth, img.Shader));
                     else
-                        self.AddIllustration(new MenuIllustration(self.menu, self, self.sceneFolder, img.Name, img.Position, false, false));
+                        self.AddIllustration(new MenuIllustration(self.menu, self, SLUGBASE_FOLDER, Path.Combine(self.sceneFolder, img.Name), img.Position, false, false));
                 }
 
                 if(self is InteractiveMenuScene interactiveScene)
