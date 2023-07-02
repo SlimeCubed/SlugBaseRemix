bc7504b6ecce6b78d5cd69886f4206735ac8566c
SlimeCubed <cubedslime@gmail.com>
2023-07-01 19:59:00 -0700

    Bump version

--- src/SlugBasePlugin.cs
+++ src/SlugBasePlugin.cs
@@ -17,7 +17,7 @@ using SlugBase.SaveData;
 
 namespace SlugBase
 {
-    [BepInPlugin("slime-cubed.slugbase", "SlugBase", "2.5.0")]
+    [BepInPlugin("slime-cubed.slugbase", "SlugBase", "2.6.0")]
     internal class SlugBasePlugin : BaseUnityPlugin
     {
         new internal static ManualLogSource Logger;
