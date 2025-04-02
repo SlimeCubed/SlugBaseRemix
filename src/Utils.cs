04b363bf7a070d8a14e065f38e9fde051e6a7ff9
Inevitabilis <ddd1020018@gmail.com>
2025-04-02 08:57:46 +0300

    Commented methods in Utils

--- src/Utils.cs
+++ src/Utils.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Name = SlugcatStats.Name;
-using MSCName = MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName;
 using UnityEngine;
 
 namespace SlugBase
 {
     internal static class Utils
     {
+
+        /// <summary>
+        /// Returns first non-null and non-negative index extenum in a collection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public static T FirstValidEnum<T>(IEnumerable<T> values)
             where T : ExtEnum<T>
         {
             return values.FirstOrDefault(val => val != null && val.Index != -1);
         }
+        /// <summary>
+        /// Filters out all non-null and non-negative extenums in a collection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public static List<T> AllValidEnums<T>(IEnumerable<T> values)
             where T : ExtEnum<T>
         {
             return values.Where(val => val != null && val.Index != -1).ToList();
         }
 
+        /// <summary>
+        /// Tries to parse text as name
+        /// </summary>
+        /// <param name="text">ID of name</param>
+        /// <returns></returns>
+        #warning unclean method
         public static Name GetName(string text)
         {
             if (!ExtEnumBase.TryParse(typeof(Name), text, true, out var res))
@@ -35,12 +53,25 @@ namespace SlugBase
             };
         }
 
+        /// <summary>
+        /// Find in ExtEnum matching ID
+        /// </summary>
+        /// <typeparam name="T">Extenum type</typeparam>
+        /// <param name="name">ID of lookup</param>
+        /// <returns>Matching ID if found, same string if not</returns>
+        #warning weird return logic
+        //it will always return same thing specified in name, be it a successful lookup or not
         public static string MatchCaseInsensitiveEnum<T>(string name)
             where T : ExtEnum<T>
         {
             return ExtEnum<T>.values.entries.FirstOrDefault(value => value.Equals(name, System.StringComparison.InvariantCultureIgnoreCase)) ?? name;
         }
 
+        /// <summary>
+        /// Parses an IEnumerable of strings into colors
+        /// </summary>
+        /// <param name="stringList"></param>
+        /// <returns>List of same size as input list. Invalid strings have UB in parsting</returns>
         public static List<Color> StringsToColors(IEnumerable<string> stringList)
         {
             if (stringList == null) return null;
@@ -53,6 +84,11 @@ namespace SlugBase
             return colorList;
         }
 
+        /// <summary>
+        /// Parses an IEnumerable of colors into a list of strings.
+        /// </summary>
+        /// <param name="colorList"></param>
+        /// <returns>List of colors same size as input</returns>
         public static List<string> ColorsToStrings(IEnumerable<Color> colorList)
         {
             if (colorList == null) return null;
